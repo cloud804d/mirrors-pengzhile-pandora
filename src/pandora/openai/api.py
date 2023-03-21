f27de616e02c05d49a0d856d2a020071d8ba00ec
pengzhile <pengzhile@gmail.com>
2023-03-21 18:18:26 +0800

    update ua
    Signed-off-by: pengzhile <pengzhile@gmail.com>

--- src/pandora/openai/api.py
+++ src/pandora/openai/api.py
@@ -105,7 +105,7 @@ class ChatGPT(API):
         }
 
         self.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                          'Chrome/109.0.0.0 Safari/537.36'
+                          'Pandora/{} Safari/537.36'.format(__version__)
         self.basic_headers = {
             'Authorization': 'Bearer ' + self.access_token,
             'User-Agent': self.user_agent,
