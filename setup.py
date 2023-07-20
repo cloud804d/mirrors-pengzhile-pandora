92f8c3622a17666e43a2a5f0e6ba4bb856758432
pengzhile <pengzhile@gmail.com>
2023-07-20 18:27:18 +0800

    upgrade pandora-cloud
    Signed-off-by: pengzhile <pengzhile@gmail.com>

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'api': requirements_api,
-        'cloud': ['pandora-cloud~=0.4.10'],
+        'cloud': ['pandora-cloud~=0.5.0'],
     },
     entry_points={
         'console_scripts': [
